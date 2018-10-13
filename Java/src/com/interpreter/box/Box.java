9be19695f6bfa86a196296dfbbfd786d1b74a778
JasperDre <jasperdre@gmail.com>
2018-10-13 11:42:06 +0200

    Added report methdo and expression

--- Java/src/com/interpreter/box/Box.java
+++ Java/src/com/interpreter/box/Box.java
@@ -43,20 +43,27 @@ public class Box {
 	 private static void run(String source) {    
 		    Scanner scanner = new Scanner(source);    
 		    List<Token> tokens = scanner.scanTokens();
+		    
+		    Parser parser = new Parser(tokens);                    
+		    Expr expression = parser.parse();
 
-		    // For now, just print the tokens.        
-		    for (Token token : tokens) {              
-		      System.out.println(token);              
-		    }                                         
+		    // Stop if there was a syntax error.                   
+		    if (hadError) return;                                  
+
+		    System.out.println(new AstPrinter().print(expression));                                       
 		  }
 	 
-	 static void error(int line, String message) {                       
-		    report(line, "", message);                                        
-		  }
-
-		  private static void report(int line, String where, String message) {
+	 private static void report(int line, String where, String message) {
 		    System.err.println(                                               
 		        "[line " + line + "] Error" + where + ": " + message);        
 		    hadError = true;                                                  
+		  }
+	 
+	 static void error(Token token, String message) {              
+		    if (token.type == TokenType.EOF) {                          
+		      report(token.line, " at end", message);                   
+		    } else {                                                    
+		      report(token.line, " at '" + token.lexeme + "'", message);
+		    }                                                           
 		  }
 }
