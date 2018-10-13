0c9c1f86cf84dbc22ccb0e29f584e3ffbc55b7df
JasperDre <jasperdre@gmail.com>
2018-10-13 18:24:42 +0200

    Added more statements

--- Java/src/com/interpreter/box/Interpreter.java
+++ Java/src/com/interpreter/box/Interpreter.java
@@ -3,6 +3,8 @@ package com.interpreter.box;
 import java.util.List;
 
 class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
+	private Environment environment = new Environment();
+	
 	void interpret(List<Stmt> statements) {
 	    try {                                
 	      for (Stmt statement : statements) {
@@ -16,6 +18,19 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	@Override                                          
 	  public Object visitLiteralExpr(Expr.Literal expr) {
 	    return expr.value;                               
+	  }
+	
+	 @Override                                          
+	  public Object visitLogicalExpr(Expr.Logical expr) {
+	    Object left = evaluate(expr.left);
+
+	    if (expr.operator.type == TokenType.OR) {        
+	      if (isTruthy(left)) return left;               
+	    } else {                                         
+	      if (!isTruthy(left)) return left;              
+	    }                                                
+
+	    return evaluate(expr.right);                     
 	  }
 	
 	@Override                                      
@@ -32,6 +47,11 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 
 	    // Unreachable.                              
 	    return null;                                 
+	  }
+	
+	@Override                                            
+	  public Object visitVariableExpr(Expr.Variable expr) {
+	    return environment.get(expr.name);                 
 	  }
 	
 	private void checkNumberOperand(Token operator, Object operand) {
@@ -87,10 +107,39 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	    stmt.accept(this);             
 	  }
 	
+	void executeBlock(List<Stmt> statements, Environment environment) {
+	    Environment previous = this.environment;                         
+	    try {                                                            
+	      this.environment = environment;
+
+	      for (Stmt statement : statements) {                            
+	        execute(statement);                                          
+	      }                                                              
+	    } finally {                                                      
+	      this.environment = previous;                                   
+	    }                                                                
+	  }
+	
+	@Override                                                     
+	  public Void visitBlockStmt(Stmt.Block stmt) {                 
+	    executeBlock(stmt.statements, new Environment(environment));
+	    return null;                                                
+	  }
+	
 	@Override                                              
 	  public Void visitExpressionStmt(Stmt.Expression stmt) {
 	    evaluate(stmt.expression);                           
 	    return null; 
+	  }
+	
+	@Override                                  
+	  public Void visitIfStmt(Stmt.If stmt) {    
+	    if (isTruthy(evaluate(stmt.condition))) {
+	      execute(stmt.thenBranch);              
+	    } else if (stmt.elseBranch != null) {    
+	      execute(stmt.elseBranch);              
+	    }                                        
+	    return null;                             
 	  }
 	
 	@Override                                    
@@ -98,6 +147,33 @@ class Interpreter implements Expr.Visitor<Object>, Stmt.Visitor<Void> {
 	    Object value = evaluate(stmt.expression);  
 	    System.out.println(stringify(value));      
 	    return null;                               
+	  }
+	
+	@Override                                     
+	  public Void visitVarStmt(Stmt.Var stmt) {     
+	    Object value = null;                        
+	    if (stmt.initializer != null) {             
+	      value = evaluate(stmt.initializer);       
+	    }
+
+	    environment.define(stmt.name.lexeme, value);
+	    return null;                                
+	  }
+	
+	@Override                                     
+	  public Void visitWhileStmt(Stmt.While stmt) { 
+	    while (isTruthy(evaluate(stmt.condition))) {
+	      execute(stmt.body);                       
+	    }                                           
+	    return null;                                
+	  }
+	
+	@Override                                        
+	  public Object visitAssignExpr(Expr.Assign expr) {
+	    Object value = evaluate(expr.value);
+
+	    environment.assign(expr.name, value);          
+	    return value;                                  
 	  }
 	
 	@Override                                        
--- Java/src/com/interpreter/box/Parser.java
+++ Java/src/com/interpreter/box/Parser.java
@@ -2,6 +2,7 @@ package com.interpreter.box;
 
 import java.util.List; 
 import java.util.ArrayList;
+import java.util.Arrays;
 
 import static com.interpreter.box.TokenType.*;
 
@@ -25,13 +26,84 @@ public class Parser {
 		  }
 	  
 	  private Expr expression() {
-		    return equality();       
+		  return assignment();       
 		  }
 	  
-	  private Stmt statement() {                  
-		    if (match(PRINT)) return printStatement();
+	  private Stmt declaration() {                
+		    try {                                     
+		      if (match(VAR)) return varDeclaration();
 
+		      return statement();                     
+		    } catch (ParseError error) {              
+		      synchronize();                          
+		      return null;                            
+		    }                                         
+		  }
+	  
+	  private Stmt statement() { 
+		  if (match(FOR)) return forStatement();
+		  	if (match(IF)) return ifStatement();
+		    if (match(PRINT)) return printStatement();
+		    if (match(WHILE)) return whileStatement();
+		    if (match(LEFT_BRACE)) return new Stmt.Block(block());
+		    
 		    return expressionStatement();             
+		  }
+	  
+	  private Stmt forStatement() {                    
+		    consume(LEFT_PAREN, "Expect '(' after 'for'.");
+
+		    Stmt initializer;                              
+		    if (match(SEMICOLON)) {                        
+		      initializer = null;                          
+		    } else if (match(VAR)) {                       
+		      initializer = varDeclaration();              
+		    } else {                                       
+		      initializer = expressionStatement();         
+		    }                          
+		    
+		    Expr condition = null;                                 
+		    if (!check(SEMICOLON)) {                               
+		      condition = expression();                            
+		    }                                                      
+		    consume(SEMICOLON, "Expect ';' after loop condition.");
+		    
+		    Expr increment = null;                                 
+		    if (!check(RIGHT_PAREN)) {                             
+		      increment = expression();                            
+		    }                                                      
+		    consume(RIGHT_PAREN, "Expect ')' after for clauses.");
+		    
+		    Stmt body = statement();
+		    
+		    if (increment != null) {               
+		        body = new Stmt.Block(Arrays.asList( 
+		            body,                            
+		            new Stmt.Expression(increment)));
+		      }
+		    
+		    if (condition == null) condition = new Expr.Literal(true);
+		    body = new Stmt.While(condition, body);
+		    
+		    if (initializer != null) {                                
+		        body = new Stmt.Block(Arrays.asList(initializer, body));
+		      }
+
+		    return body;
+		  }
+	  
+	  private Stmt ifStatement() {                                         
+		    consume(LEFT_PAREN, "Expect '(' after 'if'.");                     
+		    Expr condition = expression();                                     
+		    consume(RIGHT_PAREN, "Expect ')' after if condition."); 
+
+		    Stmt thenBranch = statement();                                     
+		    Stmt elseBranch = null;                                            
+		    if (match(ELSE)) {                                                 
+		      elseBranch = statement();                                        
+		    }                                                                  
+
+		    return new Stmt.If(condition, thenBranch, elseBranch);             
 		  }
 	  
 	  private Stmt printStatement() {                 
@@ -40,10 +112,84 @@ public class Parser {
 		    return new Stmt.Print(value);                 
 		  }
 	  
+	  private Stmt varDeclaration() {                                
+		    Token name = consume(IDENTIFIER, "Expect variable name.");
+
+		    Expr initializer = null;                                     
+		    if (match(EQUAL)) {                                          
+		      initializer = expression();                                
+		    }                                                            
+
+		    consume(SEMICOLON, "Expect ';' after variable declaration.");
+		    return new Stmt.Var(name, initializer);                      
+		  }
+
+	  private Stmt whileStatement() {                       
+		    consume(LEFT_PAREN, "Expect '(' after 'while'.");   
+		    Expr condition = expression();                      
+		    consume(RIGHT_PAREN, "Expect ')' after condition.");
+		    Stmt body = statement();
+
+		    return new Stmt.While(condition, body);             
+		  }
+	  
 	  private Stmt expressionStatement() {                 
 		    Expr expr = expression();                          
 		    consume(SEMICOLON, "Expect ';' after expression.");
 		    return new Stmt.Expression(expr);                  
+		  }
+	  
+	  private List<Stmt> block() {                      
+		    List<Stmt> statements = new ArrayList<>();
+
+		    while (!check(RIGHT_BRACE) && !isAtEnd()) {     
+		      statements.add(declaration());                
+		    }                                               
+
+		    consume(RIGHT_BRACE, "Expect '}' after block.");
+		    return statements;                              
+	  }  
+	  
+	  private Expr assignment() {                                   
+		  Expr expr = or();
+
+		    if (match(EQUAL)) {                                         
+		      Token equals = previous();                                
+		      Expr value = assignment();                                
+
+		      if (expr instanceof Expr.Variable) {                      
+		        Token name = ((Expr.Variable)expr).name;                
+		        return new Expr.Assign(name, value);                    
+		      }                                                         
+
+		      error(equals, "Invalid assignment target."); 
+		    }                                                           
+
+		    return expr;                                                
+		  }
+	  
+	  private Expr or() {                                
+		    Expr expr = and();
+
+		    while (match(OR)) {                              
+		      Token operator = previous();                   
+		      Expr right = and();                            
+		      expr = new Expr.Logical(expr, operator, right);
+		    }                                                
+
+		    return expr;                                     
+		  }
+	  
+	  private Expr and() {                               
+		    Expr expr = equality();
+
+		    while (match(AND)) {                             
+		      Token operator = previous();                   
+		      Expr right = equality();                       
+		      expr = new Expr.Logical(expr, operator, right);
+		    }                                                
+
+		    return expr;                                     
 		  }
 	  
 	  private Expr equality() {                         
@@ -177,7 +323,11 @@ public class Parser {
 
 				    if (match(NUMBER, STRING)) {                           
 				      return new Expr.Literal(previous().literal);         
-				    }                                                      
+				    }      
+				    
+				    if (match(IDENTIFIER)) {                      
+				        return new Expr.Variable(previous());       
+				      }
 
 				    if (match(LEFT_PAREN)) {                               
 				      Expr expr = expression();                            
